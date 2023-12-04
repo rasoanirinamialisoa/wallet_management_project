c28d5e392001e59dbc87800ef0e6a4821f627f4d
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-12-04 21:17:53 +0300

    Update create_table_account.sql

--- src/ressources/scripts/create_table_account.sql
+++ src/ressources/scripts/create_table_account.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Account (
-    Id VARCHAR(50) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     Name VARCHAR(50) NOT NULL ,
     balance DOUBLE PRECISION NOT NULL,
     currencyId INT REFERENCES Currency(id),
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS Account (
 );
 
 INSERT INTO Account (Id, Name, balance, currencyId, accountType) VALUES
-('123456781', 'General Account', 1000.00, 1, 'General');
+('General Account', 1000.00, 1, 'General');
 
 INSERT INTO Account (Id, Name, balance, currencyId, accountType) VALUES
-('987654321', 'Cash Account', 500.50, 1, 'Cash');
+('Cash Account', 500.50, 1, 'Cash');
 
 INSERT INTO Account (Id, Name, balance, currencyId, accountType) VALUES
-('555555555', 'My Account', 200.00, 1, 'My Account');
+( 'My Account', 200.00, 1, 'My Account');
 
 INSERT INTO Account (Id, Name, balance, currencyId, accountType) VALUES
-('111111111', 'Credit Card Account', 0.00, 1, 'Credit Card');
+( 'Credit Card Account', 0.00, 1, 'Credit Card');
