576629dc5b4668847a3f38ae02dec019be7dc732
Nantenaina <128221497+Nante1805@users.noreply.github.com>
2023-12-04 19:25:43 +0300

    fix: replace references for currency

--- src/scripts/create_table_account.sql
+++ src/scripts/create_table_account.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS Account (
     Id VARCHAR(50) PRIMARY KEY,
     Name VARCHAR(50) NOT NULL ,
     balance DOUBLE PRECISION NOT NULL,
-    currencyCode VARCHAR(3) REFERENCES Currency(currencyCode)
+    currencyId INT REFERENCES Currency(id)
     
 );
