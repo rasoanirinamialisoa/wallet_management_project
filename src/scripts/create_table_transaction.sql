9a7cde9aeb4532f4588b9b2b9714b627423903e7
Nantenaina <128221497+Nante1805@users.noreply.github.com>
2023-12-04 19:38:19 +0300

    feat : dummy data for transaction

--- src/scripts/create_table_transaction.sql
+++ src/scripts/create_table_transaction.sql
@@ -1,9 +1,21 @@
--- Script pour créer la table Transaction
 CREATE TABLE IF NOT EXISTS Transaction (
     transactionId SERIAL PRIMARY KEY,
-    accountId VARCHAR(50) REFERENCES Account(Id)
+    accountId VARCHAR(50) REFERENCES Account(Id),
     description VARCHAR(255),
     amount DOUBLE PRECISION NOT NULL,
     date TIMESTAMP NOT NULL,
-    type VARCHAR(10) NOT NULL CHECK (type IN ('DEBIT', 'CREDIT')), 
+    type VARCHAR(10) NOT NULL CHECK (type IN ('DEBIT', 'CREDIT'))
 );
+
+
+INSERT INTO Transaction (accountId, description, amount, date, type) VALUES
+('123456789', 'Purchase at XYZ Store', 50.00, CURRENT_TIMESTAMP, 'DEBIT');
+
+
+INSERT INTO Transaction (accountId, description, amount, date, type) VALUES
+('987654321', 'Salary Deposit', 2000.00, CURRENT_TIMESTAMP, 'CREDIT');
+
+
+INSERT INTO Transaction (accountId, description, amount, date, type) VALUES
+('555555555', 'Online Shopping', 75.50, CURRENT_TIMESTAMP, 'DEBIT');
+
