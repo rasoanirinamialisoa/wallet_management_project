ed55ed59e2a21bd0bd1f2e5588de4220ed1629de
Nantenaina <128221497+Nante1805@users.noreply.github.com>
2023-12-04 19:39:35 +0300

    feat: dummy data for currency

--- src/scripts/create_table_currency.sql
+++ src/scripts/create_table_currency.sql
@@ -3,3 +3,15 @@ CREATE TABLE IF NOT EXISTS Currency (
     currencyCode VARCHAR(3) NOT NULL,
     currencyName VARCHAR(50) NOT NULL
 );
+
+
+INSERT INTO Currency (currencyCode, currencyName) VALUES
+('USD', 'US Dollar');
+
+
+INSERT INTO Currency (currencyCode, currencyName) VALUES
+('EUR', 'Euro');
+
+
+INSERT INTO Currency (currencyCode, currencyName) VALUES
+('AR', 'ARIARY');
