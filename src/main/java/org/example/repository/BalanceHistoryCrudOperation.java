9b4923731244c804cf763aa2233fdb47b2be78c6
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2023-12-08 17:07:16 +0300

    Add constant variables and getter setter methods.

--- src/main/java/org/example/repository/BalanceHistoryCrudOperation.java
+++ src/main/java/org/example/repository/BalanceHistoryCrudOperation.java
@@ -1,41 +1,40 @@
-package org.example.repository;
-import models.BalanceHistory;
+ package org.example.repository;
 
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+        import java.math.BigDecimal;
+        import java.sql.Connection;
+        import java.sql.PreparedStatement;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+        import java.time.LocalDateTime;
+        import java.util.ArrayList;
+        import java.util.List;
 
-public class HistoryCrudOperation {
-        private final Connection connection;
-        public HistoryCrudOperation(Connection connection) {
+public class HistoriqueCrudOperations {
+    private static Connection connection = null;
+    public ok(Connection connection) {
         this.connection = connection;
-        }
-        public static List<BalanceHistory> getBalanceHistory (String accountId , LocalDateTime startDate ,LocalDateTime endDate ){
-            List<BalanceHistory> balanceHistories = new ArrayList<>() ;
-            String sql = "SELECT date, balance FROM BalanceHistory WHERE id_account = ? AND date BETWEEN ? AND ?";
-            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, accountId);
-                preparedStatement.setObject(2, startDate);
-                preparedStatement.setObject(3, endDate);
+    }
+    public static <BalanceHistory> List<BalanceHistory> getBalanceHistory (String accountId , LocalDateTime startDate , LocalDateTime endDate ){
+        List<BalanceHistory> balanceHistories = new ArrayList<>() ;
+        String sql = "SELECT date, balance FROM BalanceHistory WHERE id_account = ? AND date BETWEEN ? AND ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, accountId);
+            preparedStatement.setObject(2, startDate);
+            preparedStatement.setObject(3, endDate);
 
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet.next()) {
-                        LocalDateTime date = resultSet.getTimestamp("date").toLocalDateTime();
-                        BigDecimal balance = resultSet.getBigDecimal("balance");
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    LocalDateTime date = resultSet.getTimestamp("date").toLocalDateTime();
+                    BigDecimal balance = resultSet.getBigDecimal("balance");
 
-                        BalanceHistory entry = new BalanceHistory(date, balance);
-                        balanceHistories.add(entry);
-                    }
+                    BalanceHistory entry = new BalanceHistory(date, balance);
+                    balanceHistories.add(entry);
                 }
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
-            return balanceHistories;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+        return balanceHistories;
+    }
 }
